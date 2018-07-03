bb5ea6f102aa178a14f20d38bbe555494ed1bdc6
s17001 <s17001@std.it-college.ac.jp>
2018-07-03 16:22:52 +0900

    edit sumfor.go

--- my_code/golang/0703/sumFor.go
+++ my_code/golang/0703/sumFor.go
@@ -1,8 +1,17 @@
 package main
 
-//import "fmt"
+import "fmt"
 
 func main() {
+	var sum , input int
 
-	println("hoge")
+	fmt.Println("1からnまでの和")
+	fmt.Println("nの値")
+	fmt.Scan(&input)
+
+	for i:=1;i<=input;i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
 }
