3b9d4607b463083cbdc86272f724af4644991e9b
s17001 <s17001@std.it-college.ac.jp>
2018-07-04 16:18:13 +0900

    add 0704

---
+++ 0704/main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class main{
+  public static void main(String[] args) {
+      System.out.println("フィールドの大きさを入力してください(要int型)");
+	  int nums = new java.util.Scanner(System.in).nextInt();
+
+      for(int i=1;i<=nums;i++){
+
+          String line = "";
+
+          for (int o = 0 ; o<nums-i;o++){
+              line = line + " ";
+          }
+
+          for (int o=0;o!=(i-1)*2+1;o++){
+              line = line + i%10;
+          }
+
+          System.out.println(line);
+      }
+
+  }
+}
